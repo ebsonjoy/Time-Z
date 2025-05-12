6e956ebe4a679ca0f1b67a5ce2ebf0c3eb485005
ebsonjoy <ebsonjoy721@gmail.com>
2025-05-12 14:32:49 +0530

    name changed

--- routes/adminRoutes.js
+++ routes/adminRoutes.js
@@ -29,20 +29,24 @@ router.get('/unblock/:id',adminController.UserUnblock)
 
 // Admin Category
 router.get("/category",admin,adminCategoryController.adminCategory)
-router.get("/addcategory",admin,adminCategoryController.adminAddCategory)
-router.post("/submitcategory",adminCategoryController.adminNewCategory)
-router.get('/editcategory/:id',admin,adminCategoryController.adminEditCategory)
-router.post('/submiteditcategory/:id', adminCategoryController.adminUpdateCategory)
+router.route('/addcategory')
+  .get(admin, adminCategoryController.adminAddCategory)
+  .post(adminCategoryController.adminNewCategory);
+router.route('/editcategory/:id')
+  .get(admin, adminCategoryController.adminEditCategory)
+  .post(adminCategoryController.adminUpdateCategory);
 router.get('/list/:id',adminCategoryController.CategoryList)
 router.get('/unlist/:id',adminCategoryController.CategoryUnlist)
 // --------------------------------------------------------------------------
 
 // Admin Product
 router.get("/products",admin,productsController.adminProducts)
-router.get("/addproducts",admin,productsController.adminAddProducts)
-router.post("/submitproduct",upload,productsController.adminNewProducts)
-router.get("/edit/:id",productsController.adminEditProduct)
-router.post('/update/:id',upload,productsController.adminUpdateProduct)
+router.route("/addproducts")
+  .get(admin, productsController.adminAddProducts)
+  .post(upload, productsController.adminNewProducts);
+router.route("/edit/:id")
+  .get(productsController.adminEditProduct)
+  .post(upload, productsController.adminUpdateProduct);
 router.get('/publish/:id',productsController.ProductPublish)
 router.get('/unpublish/:id',productsController.ProductUnpublish)
 // --------------------------------------------------------------------------
@@ -55,12 +59,16 @@ router.post('/update_order_status',adminOrderController.updateOrderStatus);
 
 // coupon
 router.get('/coupon',admin,adminCouponController.couponPage)
-router.get('/addCoupon',admin,adminCouponController.addCoupon)
-router.post('/submitNewCoupon',adminCouponController.submitNewCoupon)
+router.route('/addCoupon')
+  .get(admin, adminCouponController.addCoupon)
+  .post(adminCouponController.submitNewCoupon);
 router.get('/listCoupon/:id',adminCouponController.couponList)
 router.get('/unlistCoupon/:id',adminCouponController.couponUnlist)
-router.get('/editCoupon/:id',admin,adminCouponController.editCoupon)
-router.post('/updateCoupon/:id',adminCouponController.updateCoupon)
+router.route('/editCoupon/:id')
+  .get(admin, adminCouponController.editCoupon)
+  .post(adminCouponController.updateCoupon);
+// --------------------------------------------------------------------------
+
 
 // dashboard
 router.get("/dashboard",admin,dashboardController.getDashboard)
@@ -74,15 +82,19 @@ router.post('/generate-report',adminController.generateReport)
 
 // brand
 router.get("/brand",admin,adminBrandController.adminBrand)
-router.get("/addBrand",admin,adminBrandController.adminAddBrand)
-router.post("/submitBrand",admin,adminBrandController.adminNewBrand)
-router.get('/editBrand/:id',adminBrandController.adminEditBrand)
-router.post('/submitEditBrand/:id', adminBrandController.adminUpdateBrand)
+router.route("/addBrand")
+  .get(admin, adminBrandController.adminAddBrand)
+  .post(admin, adminBrandController.adminNewBrand);
+router.route("/editBrand/:id")
+  .get(adminBrandController.adminEditBrand)
+  .post(adminBrandController.adminUpdateBrand);
 router.get('/listBrand/:id',adminBrandController.BrandList)
 router.get('/unlistBrand/:id',adminBrandController.BrandUnlist)
+// --------------------------------------------------------------------------
+
+
 
 // best Selling 
-
 router.get('/bestSellingProducts',admin,adminController.bestSellingProducts)
 router.get('/bestSellingCategories',admin,adminController.bestSellingCategories)
 router.get('/bestSellingBrands',admin,adminController.bestSellingBrands)
